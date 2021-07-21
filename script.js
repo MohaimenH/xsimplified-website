e66c4411bfaf2346de94edbab0ed5f9c61a4843f
MohaimenH <mohaimenhassan08@gmail.com>
2021-07-21 18:39:18 +0600

    Added Bootstrap, Animate.css

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello X, Simplified!");
+const submitButton = document.querySelector(".form-submit-btn");
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log("Clicked");
+});
